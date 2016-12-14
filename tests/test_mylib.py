93c30bc2ec55fa9a54d1c573ce429388bd733493
Alexander Kuzin <shureek@gmail.com>
2016-12-15 00:14:03 +0300

    Old code removed

--- tests/test_mylib.py
+++ tests/test_mylib.py
@@ -1,14 +1,4 @@
 import baselib
 
-'''
-def test_Sum():
-    summator = mylib.MyClass()
-    assert(summator.Sum(2, 3) == 5)
-
-def test_WrongSum():
-    summator = mylib.MyClass()
-    assert(summator.WrongSum(2, 3) == 5)
-'''
-
 def test_BaseClass():
     obj = baselib.MyBaseClass()
